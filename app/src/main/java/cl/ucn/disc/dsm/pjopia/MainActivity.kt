59fadb9c3fdef8f91d09482860d465464876ebab
Priscilla-jopia <81866435+Priscilla-jopia@users.noreply.github.com>
2021-07-20 18:04:12 -0400

    build(Libraries): upgrade the Onstart

--- app/src/main/java/cl/ucn/disc/dsm/pjopia/MainActivity.kt
+++ app/src/main/java/cl/ucn/disc/dsm/pjopia/MainActivity.kt
@@ -1,11 +1,30 @@
+/*
+ * Copyright (c) 2021. Priscilla Jopia
+ */
+
 package cl.ucn.disc.dsm.pjopia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    /**
+     * @param savedInstanceState the instance.
+     */
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("TheNews", "OnCreate!!!");
     }
+
+    override fun onStart() {
+        super.onStart();
+        Log.d("TheNews", "OnStart!!!");
+
+    }
+
+
 }
